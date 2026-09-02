c3283aa25459432d0a9b5e7b4ec232348977a508
ivandiego <ivandiego2.0@gmail.com>
2026-09-02 00:32:36 -0300

    chore: deploy (v=1788319956)

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
-import { SUPABASE_URL, PUBLISHABLE_KEY, FUNCTIONS_URL } from './config.js?v=1788317773';
+import { SUPABASE_URL, PUBLISHABLE_KEY, FUNCTIONS_URL } from './config.js?v=1788319956';
 
 export const sb = createClient(SUPABASE_URL, PUBLISHABLE_KEY);
 
