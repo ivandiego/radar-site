55b4d79187b6adad85d64008226c4a52584d90d9
ivandiego <ivandiego2.0@gmail.com>
2026-09-02 22:55:50 -0300

    chore: deploy (v=1788400550)

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-import { sessao, login, logout, fetchCarteira, registrarNoPar, fila } from './api.js?v=1788399669';
-import { diasDesde, bolaDe, alvosVivos, paresVivosDe, alertasDe, filaDoDia, metaAlvosDe } from './logic.js?v=1788399669';
-import { FUNCTIONS_URL } from './config.js?v=1788399669';
+import { sessao, login, logout, fetchCarteira, registrarNoPar, fila } from './api.js?v=1788400550';
+import { diasDesde, bolaDe, alvosVivos, paresVivosDe, alertasDe, filaDoDia, metaAlvosDe } from './logic.js?v=1788400550';
+import { FUNCTIONS_URL } from './config.js?v=1788400550';
 
 const $ = (s) => document.querySelector(s);
 let carteiras = [];
