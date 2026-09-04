a1ec55e1a25adcea703a943f1a11be7c88bcdf53
ivandiego <ivandiego2.0@gmail.com>
2026-09-03 22:44:01 -0300

    feat: F9 ultima interacao derivada da caixa (v=1788486235)

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-import { sessao, login, logout, fetchCarteira, registrarNoPar, fila, sb } from './api.js?v=1788466713';
-import { diasDesde, bolaDe, alvosVivos, paresVivosDe, alertasDe, filaDoDia, metaAlvosDe, esc, aplicarInteracoes } from './logic.js?v=1788466713';
-import { FUNCTIONS_URL } from './config.js?v=1788466713';
+import { sessao, login, logout, fetchCarteira, registrarNoPar, fila, sb } from './api.js?v=1788486235';
+import { diasDesde, bolaDe, alvosVivos, paresVivosDe, alertasDe, filaDoDia, metaAlvosDe, esc, aplicarInteracoes } from './logic.js?v=1788486235';
+import { FUNCTIONS_URL } from './config.js?v=1788486235';
 
 const $ = (s) => document.querySelector(s);
 let carteiras = [];
